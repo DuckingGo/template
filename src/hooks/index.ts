ecfefe170d649327f5c7ae3cb8217be260ff32ee
DuckingGo <kuliah.informatika01@gmail.com>
2025-08-04 19:49:43 +0700

    feat(add): detect system localstorage, theme, toast, make it easier to call in index

---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+export { useTheme } from './use-theme'
+export { useLocalStorage } from './use-local-storage'
+export { useToast } from './use-toast'
---
+++ src/hooks/use-toast.ts
@@ -0,0 +1,20 @@
+import { toast } from 'sonner'
+
+export const useToast = () => {
+  return {
+    toast: (message: string) => toast(message),
+    success: (message: string) => toast.success(message),
+    error: (message: string) => toast.error(message),
+    warning: (message: string) => toast.warning(message),
+    info: (message: string) => toast.info(message),
+    loading: (message: string) => toast.loading(message),
+    promise: <T>(
+      promise: Promise<T>,
+      messages: {
+        loading: string
+        success: string | ((data: T) => string)
+        error: string | ((error: any) => string)
+      }
+    ) => toast.promise(promise, messages),
+  }
+}
